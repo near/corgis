62499a5c0cba80e8e07f2f29b9e7f52547f46d88
Zahhar Kirillov <zahhar@gmail.com>
2021-02-10 18:44:46 +0100

    Closes #45 (#53)
    * Issue #45 fixed

--- src/config.js
+++ src/config.js
@@ -3,39 +3,26 @@ const CONTRACT_NAME = process.env.CONTRACT_NAME || 'new-corgis';
 
 function getConfig(env) {
   switch (env) {
-    case 'mainnet':
+    case 'production':
       return {
         networkId: 'mainnet',
         nodeUrl: 'https://rpc.mainnet.near.org',
         contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.mainnet.near.org',
-        helperUrl: 'https://helper.mainnet.near.org',
+        walletUrl: 'https://wallet.mainnet.near.org'
       };
     case 'development':
-    case 'production':
-    case 'testnet':
       return {
-        networkId: 'default',
+        networkId: 'testnet',
         nodeUrl: 'https://rpc.testnet.near.org',
         contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.testnet.near.org',
-        helperUrl: 'https://helper.testnet.near.org',
-      };
-    case 'devnet':
-      return {
-        networkId: 'devnet',
-        nodeUrl: 'https://rpc.devnet.near.org',
-        contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.devnet.near.org',
-        helperUrl: 'https://helper.devnet.near.org',
+        walletUrl: 'https://wallet.testnet.near.org'
       };
     case 'betanet':
       return {
         networkId: 'betanet',
         nodeUrl: 'https://rpc.betanet.near.org',
         contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.betanet.near.org',
-        helperUrl: 'https://helper.betanet.near.org',
+        walletUrl: 'https://wallet.betanet.near.org'
       };
     case 'local':
       return {
@@ -43,22 +30,7 @@ function getConfig(env) {
         nodeUrl: 'http://localhost:3030',
         keyPath: `${process.env.HOME}/.near/validator_key.json`,
         walletUrl: 'http://localhost:4000/wallet',
-        contractName: CONTRACT_NAME,
-      };
-    case 'test':
-    case 'ci':
-      return {
-        networkId: 'shared-test',
-        nodeUrl: 'https://rpc.ci-testnet.near.org',
-        contractName: CONTRACT_NAME,
-        masterAccount: 'test.near',
-      };
-    case 'ci-betanet':
-      return {
-        networkId: 'shared-test-staging',
-        nodeUrl: 'https://rpc.ci-betanet.near.org',
-        contractName: CONTRACT_NAME,
-        masterAccount: 'test.near',
+        contractName: CONTRACT_NAME
       };
     default:
       throw Error(
