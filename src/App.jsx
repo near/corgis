be365f453d660529cd9c6b05f29846db45bdd4d5
Taras Moskalenko <moskalenko.taras.d@gmail.com>
2021-02-10 18:30:18 +0200

    Fixup/51_react-router-deployment-bug (#52)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import 'regenerator-runtime/runtime';
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
 
 import Header from './component/Header/Header';
@@ -8,7 +8,7 @@ import Footer from './component/Footer/Footer';
 
 const App = () => (
   <div className='App'>
-    <Router>
+    <Router hashType='noslash'>
       <Header />
       <Routes />
       <Footer />
