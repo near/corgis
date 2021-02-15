4d7d86aa619f4141f900ba257da5ecca80dcfe9f
Taras Moskalenko <moskalenko.taras.d@gmail.com>
2021-02-15 14:22:30 +0200

    Refactor/67 spinner remake (#70)
    * deleted white background from image
    
    * refactored spinner with image import and BEM

--- src/modules/common/Spinner/Spinner.jsx
+++ src/modules/common/Spinner/Spinner.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 
 import './Spinner.scss';
 
-const Spinner = () => <div className='spinner'></div>;
+import corgiCircle from '~assets/images/corgi-circle.png';
+
+const Spinner = () => (
+  <div className='spinner-wrapper'>
+    <div className='spinner'>
+      <div className='spinner__gradient'></div>
+      <div className='spinner__background'></div>
+      <img className='spinner__corgi' src={corgiCircle}></img>
+    </div>
+  </div>
+);
 
 export default Spinner;
