6dd76215e0eef31527b43ace3b2383b0986a7992
Taras Moskalenko <moskalenko.taras.d@gmail.com>
2021-03-02 17:46:55 +0200

    Features/175 use react color instead of html colorpicker (#192)
    * added react-color
    
    * refactored using SketchPicker component provided by react-color

--- src/modules/common/Colorpicker/Colorpicker.jsx
+++ src/modules/common/Colorpicker/Colorpicker.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import './Colorpicker.scss';
+
+import classNames from 'classnames';
+
+import { SketchPicker } from 'react-color';
+
+import { useDetectClickOutside } from '~hooks/';
 
 const ColorpickerPropTypes = {
   title: PropTypes.string,
@@ -10,15 +16,42 @@ const ColorpickerPropTypes = {
 };
 
 const Colorpicker = ({ title, color, pickColor }) => {
-  const handleColor = (event) => {
-    pickColor(event.target.value);
+  const buttonRef = useRef(null);
+
+  const [isOpened, setIsOpened] = useState(false);
+  const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+  const openColorpicker = (event) => {
+    event.preventDefault();
+
+    if (buttonRef && buttonRef.current && event.target === buttonRef.current) {
+      setCoords({ x: event.nativeEvent.offsetX, y: event.nativeEvent.offsetY });
+    }
+
+    setIsOpened(true);
   };
+
+  const handleColor = (color) => {
+    pickColor(color.hex);
+  };
+
+  useDetectClickOutside(buttonRef, () => setIsOpened(false));
 
   return (
     <div className='colorpicker'>
-      <label className='colorpicker__result' style={{ backgroundColor: color }}>
-        <input className='colorpicker__input' type='color' value={color} onChange={(event) => handleColor(event)} />
-      </label>
+      <button
+        ref={buttonRef}
+        className='colorpicker__button'
+        onClick={(event) => openColorpicker(event)}
+        style={{ backgroundColor: color }}
+      >
+        <div
+          className={classNames('colorpicker__window', { 'colorpicker__window--opened': isOpened })}
+          style={{ top: coords.y, left: coords.x }}
+        >
+          <SketchPicker color={color} onChange={handleColor} disableAlpha />
+        </div>
+      </button>
 
       <p className='colorpicker__title'>{title || 'Colorpicker'}</p>
     </div>
