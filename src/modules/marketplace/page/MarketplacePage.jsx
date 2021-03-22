394a2f1855f697c6686c2c1cc3233b9190cd4acd
Aliaksandr Padhainy <sxurik@gmail.com>
2021-03-22 16:55:50 +0300

    Sort corgis in marketplace depending on auction end

--- src/modules/marketplace/page/MarketplacePage.jsx
+++ src/modules/marketplace/page/MarketplacePage.jsx
@@ -6,6 +6,18 @@ import { MarketplaceContext } from '~contexts';
 
 import { CorgisShowCase } from '~modules/common';
 
+import { formatToMs } from '~helpers/time';
+
+const hasAuctionEnded = ({ for_sale: { expires } }) => (new Date()).getTime() > formatToMs(expires);
+
+const sortCorgisAsc = ({ for_sale: { expires: expiresA } }, { for_sale: { expires: expiresB } }) => expiresA - expiresB;
+const sortCorgisDes = ({ for_sale: { expires: expiresA } }, { for_sale: { expires: expiresB } }) => expiresB - expiresA;
+
+const getCorgisForSaleSorted = (corgisForSale) => ([
+  ...corgisForSale.filter((corgi) => corgi && !hasAuctionEnded(corgi)).sort(sortCorgisAsc),
+  ...corgisForSale.filter((corgi) => corgi && hasAuctionEnded(corgi)).sort(sortCorgisDes),
+]);
+
 const MarketplacePage = () => {
   const { corgisForSale, getCorgisForSale } = useContext(MarketplaceContext);
 
@@ -15,7 +27,7 @@ const MarketplacePage = () => {
 
   return (
     <div className='marketplace'>
-      <CorgisShowCase corgis={corgisForSale} title='For Sale' showAuctionInfo />
+      <CorgisShowCase corgis={getCorgisForSaleSorted(corgisForSale)} title='For Sale' showAuctionInfo />
     </div>
   );
 };
